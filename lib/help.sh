f069adaec3a5a0fc9f6146eb60c6f30aad6b7a68
CodingAxolotl <superkalikimaka@gmail.com>
2026-01-15 21:34:11 +0000

    Delete lib directory

--- lib/help.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "🐍 Creeper CLI"
-echo ""
-echo "Available commands:"
-echo ""
-echo "  creeper install img<number>  - Download an image from GitHub into Downloads"
-echo "  creeper update               - Check for updates and upgrade if needed"
-echo "  creeper help                 - Show this help message"
